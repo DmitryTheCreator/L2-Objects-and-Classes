d6b81a3ffd446a6aad48bf58d36e5d9c9a1074be
DmitryTheCreator <dmitriyzamorev@gmail.com>
2020-10-29 21:47:15 +0500

    V 1.0
    Создал класс Car

---
+++ L2 Objects and Classes/L2 Objects and Classes.cpp
@@ -0,0 +1,42 @@
+﻿#include "iostream"
+#include "conio.h"
+
+class Car {
+protected:
+	int speed, weight;
+public:
+	Car() {
+		speed = 100;
+		weight = 2000;
+		printf("Вызван конструктор Car()\n");
+		printf("Скорость машины = %d, Вес машины = %d\n", speed, weight);
+	}
+	Car(int speed, int weight) {
+		this->speed = speed;
+		this->weight = weight;
+		printf("Вызван конструктор Car(int speed, int weight)\n");
+		printf("Скорость машины = %d, Вес машины = %d\n", speed, weight);
+	}
+	Car(const Car& c) {
+		speed = c.speed;
+		weight = c.weight;
+		printf("Вызван конструктор Car(const Car& c)\n");
+		printf("Скорость машины = %d, Вес машины = %d\n", speed, weight);
+	}
+	~Car() {
+		printf("Вызван дестуктор ~Car()\n");
+		//printf("Скорость машины = %d, Вес машины = %d\n", speed, weight);
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "");
+	{
+		Car lada;
+		Car* porsh = new Car(100, 3000);
+		Car* lamba = new Car(*porsh);
+		
+		delete porsh;
+		delete lamba;
+	}
+}
