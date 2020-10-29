1d4e3387cea6b9179fc33e4ff73284ab2498b031
DmitryTheCreator <dmitriyzamorev@gmail.com>
2020-10-29 22:22:55 +0500

    Beta 2.0
    Добавил наследуемый класс Bus от Car

--- L2 Objects and Classes/L2 Objects and Classes.cpp
+++ L2 Objects and Classes/L2 Objects and Classes.cpp
@@ -9,34 +9,72 @@ public:
 		speed = 100;
 		weight = 2000;
 		printf("Вызван конструктор Car()\n");
-		printf("Скорость машины = %d, Вес машины = %d\n", speed, weight);
+		printf("Скорость = %d, Вес = %d\n", speed, weight);
 	}
 	Car(int speed, int weight) {
 		this->speed = speed;
 		this->weight = weight;
 		printf("Вызван конструктор Car(int speed, int weight)\n");
-		printf("Скорость машины = %d, Вес машины = %d\n", speed, weight);
+		printf("Скорость = %d, Вес = %d\n", speed, weight);
 	}
 	Car(const Car& c) {
 		speed = c.speed;
 		weight = c.weight;
 		printf("Вызван конструктор Car(const Car& c)\n");
-		printf("Скорость машины = %d, Вес машины = %d\n", speed, weight);
+		printf("Скорость = %d, Вес = %d\n", speed, weight);
 	}
 	~Car() {
 		printf("Вызван дестуктор ~Car()\n");
-		//printf("Скорость машины = %d, Вес машины = %d\n", speed, weight);
 	}
+};
+
+class Bus : public Car {
+protected:
+	int price;
+public: 
+	Bus() {
+		speed = 60;
+		weight = 7000;
+		price = 30;
+		printf("Вызван конструктор Bus()\n");
+		printf("Скорость = %d, Вес = %d, Стоимость проезда = %d\n", speed, weight, price);
+	}
+	Bus(int speed, int weight, int price) : Car(speed, weight) {
+		this->price = price;
+		printf("Вызван конструктор Bus(int speed, int weight)\n");
+		printf("Скорость = %d, Вес = %d, Стоимость проезда = %d\n", speed, weight, price);
+	}
+	Bus(const Bus& b) {
+		speed = b.speed;
+		weight = b.weight;
+		price = b.price;
+		printf("Вызван конструктор Bus(const Bus& c)\n");
+		printf("Скорость = %d, Вес = %d, Стоимость проезда = %d\n", speed, weight, price);
+	}
+	~Bus() {
+		printf("Вызван дестуктор ~Bus()\n");
+	}
+
 };
 
 int main() {
 	setlocale(LC_ALL, "");
 	{
 		Car lada;
-		Car* porsh = new Car(100, 3000);
+		Car* porsh = new Car(130, 3000);
 		Car* lamba = new Car(*porsh);
 		
+		printf("\n\n");
+
+		Bus uaz(50, 8000, 30);
+		Bus* uazik = new Bus();
+		Bus* uazblin = new Bus(uaz);
+
+		printf("\n\n");
+
 		delete porsh;
 		delete lamba;
+		delete uazik;
+		delete uazblin;
 	}
 }
